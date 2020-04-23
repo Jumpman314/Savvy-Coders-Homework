5a2cba49020c040c3a94a7614d2095411b6d6f71
Joseph Jordan <jumpman.fam314@gmail.com>
2020-04-23 14:40:46 -0500

    started week3 homework

---
+++ JS-Week3-Homework/index.js
@@ -0,0 +1,13 @@
+const pizzaPlace = "Joey's Pies";
+
+let pizzaToppings = ["Sausage", "Pepperoni", "Jalapeno", "Bacon"];
+
+function greetCustomer() {
+    console.log(`"Welcome to ${pizzaPlace}, our toppings are ${pizzaToppings}."`);
+};
+
+function getPizzaOrder();
+
+function preparePizza();
+
+function servePizza();
