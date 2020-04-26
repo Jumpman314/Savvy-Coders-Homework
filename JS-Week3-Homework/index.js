7221645728bc0aead8c8b9477783a22a48066a6d
Joseph Jordan <jumpman.fam314@gmail.com>
2020-04-25 21:24:42 -0500

    Partially completed week3 Homework :(

--- JS-Week3-Homework/index.js
+++ JS-Week3-Homework/index.js
@@ -1,22 +1,27 @@
 const pizzaPlace = "Joey's Pies";
 let pizzaToppings = ["Sausage", "Pepperoni", "Jalapeno", "Bacon"];
-let size = ["Small", "Medium", "Large"];
-let crust = ["Thin", "Stuffed", "Garlic Butter"];
+let pizzaSize = ["Small", "Medium", "Large"];
+let pizzaCrust = ["Thin", "Stuffed", "Garlic"];
 
 let greetCustomer = function() {
   return `"Welcome to ${pizzaPlace}, our toppings are ${pizzaToppings}."`;
 };
 console.log(greetCustomer());
 
-function getPizzaOrder(size, crust, pizzaToppings) {
-  console.log(
-    `One ${size} ${crust} crust pizza with ${pizzaToppings} coming right up!`
-  );
-}
-console.log(getPizzaOrder);
+let obj = {
+  toppings: ["Sausage", "Pepperoni", "Jalapeno", "Bacon"],
+  size: ["Small", "Medium", "Large"],
+  crust: ["Thin", "Stuffed", "Garlic"]
+};
 
-for (let toppings of pizzaToppings) {
-  console.log(toppings);
+function getPizzaOrder() {
+  return `One ${obj.size[1]} ${obj.crust[1]} crust pizza with ${obj.toppings[3]}, and ${obj.toppings[2]} coming right up!`;
 }
+console.log(getPizzaOrder());
+console.log(obj);
 
-console.log(greetCustomer());
+function preparePizza(size, crust, toppings) {
+  console.log(obj.size[1] + obj.crust[1] + obj.toppings[3]);
+  return "...Cooking Pizza...";
+}
+console.log(preparePizza());
